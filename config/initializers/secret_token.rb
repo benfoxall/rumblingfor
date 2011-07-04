b2bd9701a0e612e65f3a62deb37182d99cfc8ae0
Ben Foxall <ben@whiteoctober.co.uk>
2011-07-04 22:25:54 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rumblingfor::Application.config.secret_token = '9d3de4e44e80274fc4eb118f698ddb55dd6472e995fb6ad91b1f99fa8241507c07ccc2e35c833f8273a0db46599775de3538db4bc250b1b0e50baa4bf8cb5eb1'
